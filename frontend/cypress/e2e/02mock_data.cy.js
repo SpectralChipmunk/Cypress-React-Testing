099e6487433fd41294cc984cb3df352a3560ec4a
ishan-me <ishan02016@gmail.com>
2022-08-01 15:21:39 +0545

    added a cart functionality with mocak data using fixtures and custom commands

--- frontend/cypress/e2e/02mock_data.cy.js
+++ frontend/cypress/e2e/02mock_data.cy.js
@@ -2,5 +2,13 @@ describe('Mock some data', () => {
 	it.only('Check the all items api call', () => {
 		cy.populateData();
 		cy.get('[data-cy=item-list]').children().should('have.length', 5);
+		cy.get('[data-cy=item-list] > :nth-child(1) button').click();
+		cy.get('[data-cy=item-list] > :nth-child(3) button').click();
+		cy.get('[data-cy=item-list] > :nth-child(5) button').click();
+		cy.get('[data-cy=shopping-badge]').as('shopping-badge');
+		cy.get('@shopping-badge').contains(3);
+		cy.get('[data-cy=cart-button]').click();
+		cy.get('[data-cy=added-to-cart]').children().should('have.length', 3);
+		cy.get('[data-cy=shop-name]').click();
 	})
 })
